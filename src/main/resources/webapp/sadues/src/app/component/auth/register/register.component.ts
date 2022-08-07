14dde6c69c40aeb7a2957849c9f30af9b49a0875
Elroy Kanye <elroykanye@gmail.com>
2022-08-07 15:41:31 +0100

    update register component form

--- src/main/resources/webapp/sadues/src/app/component/auth/register/register.component.ts
+++ src/main/resources/webapp/sadues/src/app/component/auth/register/register.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Gender} from "../../../model/user.model";
+import {Gender, User, UserRegister} from "../../../model/user.model";
 import {University} from "../../../model/university.model";
+import {Router} from "@angular/router";
+import {AcademicYearService} from "../../../service/academic-year.service";
+import {AuthService} from "../../../service/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -13,20 +16,40 @@ export class RegisterComponent implements OnInit {
   genders: string[] = Object.keys(Gender);
   universities: University[] = [];
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+
+  ) {
     this.registerForm = this.fb.group({
-      username: ['', Validators.required, Validators.email],
-      password: ['', Validators.required],
-      regno: ['', Validators.required],
-      name: ['', Validators.required],
-      university: [0, Validators.required],
+      email: ['', [Validators.required, Validators.email]], password: ['', Validators.required],
+      regno: ['', Validators.required], name: ['', Validators.required],
+      university: [0, Validators.required], gender: [Gender.OTHER, Validators.required]
     });
+
   }
 
   ngOnInit(): void {
   }
 
   registerAction() {
-
+    const user: User = new User(
+      0,
+      this.registerForm.get('email')?.value,
+      this.registerForm.get('regno')?.value,
+      this.registerForm.get('name')?.value,
+      this.registerForm.get('role')?.value,
+      this.registerForm.get('gender')?.value,
+    );
+    const userRegister: UserRegister = new UserRegister(
+      this.registerForm.get('email')?.value, this.registerForm.get('email')?.value, user
+    );
+    console.log(userRegister);
+    /*
+    this.authService.register(userRegister).subscribe((res) => {
+      this.router.navigate(['/login']).then();
+    });
+     */
   }
 }
