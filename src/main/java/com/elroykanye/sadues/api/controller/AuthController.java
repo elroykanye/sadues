cb7cb62ba49aa3bec6fdd8464af3deba8c400a4d
Elroy Kanye <elroykanye@gmail.com>
2022-09-04 04:51:38 +0100

    check auth by returning boolean

--- src/main/java/com/elroykanye/sadues/api/controller/AuthController.java
+++ src/main/java/com/elroykanye/sadues/api/controller/AuthController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,5 +45,10 @@ public class AuthController {
     @PostMapping(value = "/logout")
     public ResponseEntity<SaResponse> logout(@Valid @RequestBody UserDto.UserLogin userLogin, HttpSession httpSession) {
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/check")
+    public ResponseEntity<Boolean> check(HttpSession httpSession) {
+        return ResponseEntity.ok(true);
     }
 }
