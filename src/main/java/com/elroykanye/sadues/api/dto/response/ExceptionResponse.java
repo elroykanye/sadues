6bbe23bea911ab07cb1b1e46403292b6cbd10e8c
Elroy Kanye <elroykanye@gmail.com>
2022-09-07 04:55:03 +0100

    add errors to ExceptionResponse

--- src/main/java/com/elroykanye/sadues/api/dto/response/ExceptionResponse.java
+++ src/main/java/com/elroykanye/sadues/api/dto/response/ExceptionResponse.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 
 public record ExceptionResponse (
         String message,
-        String path
+        String path,
+        Object errors
 
 ) implements Serializable {
 
