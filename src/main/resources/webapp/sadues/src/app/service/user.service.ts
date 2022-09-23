244c1e0d4e435fa4cf4713979d0450d6324700bf
Elroy Kanye <elroykanye@gmail.com>
2022-09-23 05:50:10 +0100

    get user by email in user.service.ts

--- src/main/resources/webapp/sadues/src/app/service/user.service.ts
+++ src/main/resources/webapp/sadues/src/app/service/user.service.ts
@@ -1,6 +1,8 @@
 import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {USER_API_URL} from "../app.constants";
+import {User} from "../model/user.model";
+import {SaResponse} from "../model/response/sa-response";
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,10 @@ export class UserService {
     private http: HttpClient
   ) {
   }
+
+  getByEmail = (email: string) => this.http.get<User>(`${this.apiUrl}/user`, {
+    params: {email: email}
+  });
+
+  update = (user: User) => this.http.put<SaResponse>(`${this.apiUrl}`, user);
 }
