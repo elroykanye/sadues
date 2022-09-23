24475366ea8cb5ffed9d31d658e5248f59c8a532
Elroy Kanye <elroykanye@gmail.com>
2022-09-23 05:49:53 +0100

    get membership by key in membership.service.ts

--- src/main/resources/webapp/sadues/src/app/service/membership.service.ts
+++ src/main/resources/webapp/sadues/src/app/service/membership.service.ts
@@ -1,6 +1,6 @@
 import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Membership} from "../model/membership.model";
+import {Membership, MembershipKey} from "../model/membership.model";
 import {Observable} from "rxjs";
 import {SaResponse} from "../model/response/sa-response";
 import {MEMBERSHIP_API_URL} from "../app.constants";
@@ -18,8 +18,9 @@ export class MembershipService {
 
   create = (membership: Membership): Observable<SaResponse> => this.http.post<SaResponse>(this.apiUrl, membership);
   getAll = (): Observable<Membership[]> => this.http.get<Membership[]>(this.apiUrl);
-  getById = (id: number): Observable<Membership> => this.http.get<Membership>(`${this.apiUrl}/${id}`);
+  getById = (key: MembershipKey): Observable<Membership> => this.http.get<Membership>(`${this.apiUrl}/key`,
+    {params: {userId: key.userId, associationId: key.associationId}}
+  );
   update = (membership: Membership): Observable<SaResponse> => this.http.put<SaResponse>(this.apiUrl, membership);
   delete = (id: number): Observable<void> => this.http.delete<void>(`${this.apiUrl}/${id}`);
-
 }
