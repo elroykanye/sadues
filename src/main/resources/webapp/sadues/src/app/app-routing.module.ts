620aba611e6d57343097ea488603ff85385fcbb8
Elroy Kanye <elroykanye@gmail.com>
2022-09-26 05:53:46 +0100

    configure routing under main component

--- src/main/resources/webapp/sadues/src/app/app-routing.module.ts
+++ src/main/resources/webapp/sadues/src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import {RegisterComponent} from "./component/auth/register/register.component";
 import {AccountComponent} from "./component/dashboard/account/account.component";
 import {HomeComponent} from "./component/dashboard/home/home.component";
 import {PayDuesComponent} from "./component/dashboard/pay-dues/pay-dues.component";
+import {AuthGuard} from "./guard/auth.guard";
+import {MainComponent} from "./component/nav/main/main.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'pay', component: PayDuesComponent},
-  {path: 'account', component: AccountComponent},
+  {path: '', component: MainComponent, canActivate: [AuthGuard], children: [
+      {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+      {path: 'pay', component: PayDuesComponent, canActivate: [AuthGuard]},
+      {path: 'account', component: AccountComponent, canActivate: [AuthGuard]},
+    ]
+  },
 ];
 
 @NgModule({
