e62f4695a85eb2fcd71db631b1645ad6ccf047e1
Elroy Kanye <elroykanye@gmail.com>
2022-09-30 19:09:52 +0100

    create user repo interface method to find all by role

--- sadues-server/src/main/java/com/elroykanye/sadues/data/repository/UserRepository.java
+++ sadues-server/src/main/java/com/elroykanye/sadues/data/repository/UserRepository.java
@@ -1,14 +1,18 @@
 package com.elroykanye.sadues.data.repository;
 
 import com.elroykanye.sadues.data.entity.User;
+import com.elroykanye.sadues.data.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    List<User> findAllByRole(Role role);
 
     boolean existsByEmail(String email);
 }
